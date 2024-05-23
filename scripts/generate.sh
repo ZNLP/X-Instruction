fbac4b3cad05c02c8fa4b889a29caaf9091cbcf2
james-yw <yw1134324372@gmail.com>
2024-05-23 18:01:35 +0800

    Modify the generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,7 +3,7 @@ LANGUAGES= # array of languages, e.g. ("bn")
 input_file= # path to input question file, e.g. ./data/vicuna-eval/bn/question.jsonl
 
 for lang in "${LANGUAGES[@]}";do
-    python -u utils/chatgpt_generation.py \
+    python -u utils/chatgpt_generate.py \
     --input_file ${input_file} \
     --lang ${lang}
 done
